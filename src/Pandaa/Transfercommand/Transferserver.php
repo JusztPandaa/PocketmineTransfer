b43bdb857701b386b6dc963d5679bdca6f31f48d
DevPandaa <70394019+JusztPandaa@users.noreply.github.com>
2021-04-08 22:30:48 -0400

    Add files via upload

---
+++ src/Pandaa/Transfercommand/Transferserver.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Pandaa\Transfercommand;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\command\{Command,CommandSender};
+
+class Transferserver extends PluginBase implements Listener {
+
+    public function onEnable() {
+
+       $this->getServer()->getPluginManager()->registerEvent($this, $this);
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args){
+        switch($cmd->getName()){
+            case "transferserver":
+
+                if(count($args) < 1){
+                    throw new InvalidCommandSyntaxException();
+                }elseif(!($sender instanceof Player)){
+                    $sender->sendMessage("This command must be executed as a player");
+
+                    return false;
+                }
+
+                $sender->transfer($args[0], (int) ($args[1] ?? 19132));
+
+                return true;
+
+
+
+
+
+        }
+    }
+}
